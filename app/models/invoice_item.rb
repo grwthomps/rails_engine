218fe19d6968db2495cf898e5ab17b38f122ad27
Graham Thompson <grwthomps@gmail.com>
2019-11-19 15:40:41 -0700

    Create convert_unit_price model methods for item and invoice_item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,4 +5,9 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
 
+  before_save :convert_unit_price
+
+  def convert_unit_price
+    self.unit_price /= 100
+  end
 end
