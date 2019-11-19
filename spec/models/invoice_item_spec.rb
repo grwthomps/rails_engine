b5989fdfe04288de43802986593188cbf61bb747
Graham Thompson <grwthomps@gmail.com>
2019-11-19 15:40:06 -0700

    Create model tests for converting unit_price cents to dollars for item and invoice_item

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -12,3 +12,15 @@ RSpec.describe InvoiceItem, type: :model do
     it {should belong_to :item}
   end
 
+  describe 'instance methods' do
+    it 'converts unit price to dollars' do
+      merch = Merchant.create!(name: "Bob's Donuts")
+      item = Item.create!(name: "Jelly filled donut", description: "It's got jelly!", unit_price: 75901, merchant_id: merch.id)
+      cust = Customer.create!(first_name: "Joe", last_name: "Smith")
+      inv = Invoice.create!(customer_id: cust.id, merchant_id: merch.id, status: "shipped")
+      InvoiceItem.create!(item_id: item.id, invoice_id: inv.id, quantity: 5, unit_price: 13635)
+
+      expect(InvoiceItem.last.unit_price).to eq(136.35)
+    end
+  end
+end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,3 +14,12 @@ RSpec.describe Item, type: :model do
     it {should have_many(:invoices).through(:invoice_items)}
   end
 
+  describe 'instance methods' do
+    it 'converts unit price to dollars' do
+      bob = Merchant.create!(name: "Bob's Donuts")
+      Item.create!(name: "Jelly filled donut", description: "It's got jelly!", unit_price: 75901, merchant_id: bob.id)
+
+      expect(Item.last.unit_price).to eq(759.01)
+    end
+  end
+end
