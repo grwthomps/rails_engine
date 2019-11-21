3b26276b7961970158165d275456e8ca71a7aa44
Graham Thompson <grwthomps@gmail.com>
2019-11-21 08:50:31 -0700

    Add merchant routes for index, show, and find

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index]
+      get '/merchants/find', to: 'merchants/search#find'
+      resources :merchants, only: [:index, :show]
     end
   end
 end
