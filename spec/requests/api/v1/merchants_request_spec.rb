7c058ae9d4c86b040fff48018c7f73d44ded9875
Graham Thompson <grwthomps@gmail.com>
2019-11-21 08:48:50 -0700

    Add tests for finding merchants by id, name, and date

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -11,7 +11,45 @@ describe 'Merchants API' do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body)
+    expect(merchants["data"].count).to eq(3)
+  end
 
-    expect(merchants.count).to eq(3)
+  it 'can get one merchant by its id' do
+    bob = Merchant.create(name: "Bob's Donuts")
+    jenny = Merchant.create(name: "Jenny's Fabulous Flowers")
+
+    get "/api/v1/merchants/#{bob.id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"]["id"]).to eq(bob.id.to_s)
+  end
+
+  it 'can find a merchant by name' do
+    bob = Merchant.create(name: "Bob's Donuts")
+    jenny = Merchant.create(name: "Jenny's Fabulous Flowers")
+
+    get '/api/v1/merchants/find?name=Bob%27s%20Donuts'
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"]["id"]).to eq(bob.id.to_s)
+    expect(merchant["data"]["attributes"]["name"]).to eq(bob.name)
+  end
+
+  it 'can find a merchant by date' do
+    bob = Merchant.create(name: "Bob's Donuts")
+    jenny = Merchant.create(name: "Jenny's Fabulous Flowers", created_at: "Thurs, 21 Nov 2019 18:49:17 UTC +00:00")
+
+    get "/api/v1/merchants/find?created_at=#{jenny.created_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"]["id"]).to eq(jenny.id.to_s)
+  end
+
   end
 end
