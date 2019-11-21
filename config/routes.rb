39f17ca37a02825122d8b23562bafc92c8609712
Graham Thompson <grwthomps@gmail.com>
2019-11-21 09:09:11 -0700

    Add merchant routes for find_all and random

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find', to: 'merchants/search#find'
+      get '/merchants/find_all', to: 'merchants/search#find_all'
+      get '/merchants/random', to: 'merchants/random#show'
       resources :merchants, only: [:index, :show]
     end
   end
