65c5daa580dd21738e7cd2128591b9a3a2a2e2e1
Graham Thompson <grwthomps@gmail.com>
2019-11-21 10:24:14 -0700

    Refactor routes to use namespace merchant for find, find_all, and random

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/merchants/find', to: 'merchants/search#find'
-      get '/merchants/find_all', to: 'merchants/search#find_all'
-      get '/merchants/random', to: 'merchants/random#show'
+      namespace :merchants do
+        get '/find', to: 'search#find'
+        get '/find_all', to: 'search#find_all'
+        get '/random', to: 'random#show'
+      end
       resources :merchants, only: [:index, :show]
     end
   end
