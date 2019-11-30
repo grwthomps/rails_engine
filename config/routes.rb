fc546fc8f1ab7df78f4998778308103c10691126
Graham Thompson <grwthomps@gmail.com>
2019-11-30 08:56:14 -0700

    Add namespaced customer random route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       namespace :customers do
         get '/find', to: 'search#find'
         get '/find_all', to: 'search#find_all'
+        get '/random', to: 'random#show'
       end
       resources :customers, only: [:index, :show]
     end
