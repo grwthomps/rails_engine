6e50d156e2e9deed8e2737059b93a1a10e55cd82
Graham Thompson <grwthomps@gmail.com>
2019-11-30 07:16:33 -0700

    Add total_revenue_on_date class method to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,4 +8,11 @@ class Invoice < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   has_many :transactions
+
+  def self.total_revenue_on_date(date)
+    joins(:invoice_items, :transactions)
+    .where("result = 'success'")
+    .where(created_at: date.to_date.all_day)
+    .sum('quantity * unit_price')
+  end
 end
