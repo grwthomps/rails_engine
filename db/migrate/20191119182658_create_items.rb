d30c28f55d847f89db13701cc43fcc86c9a472a3
Graham Thompson <grwthomps@gmail.com>
2019-12-02 07:11:46 -0700

    Refactor database migration to use citext instead of strings to allow for case-insensitive search

--- db/migrate/20191119182658_create_items.rb
+++ db/migrate/20191119182658_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
+      t.citext :name
       t.text :description
       t.float :unit_price
       t.references :merchant, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,12 @@
 ActiveRecord::Schema.define(version: 2019_11_19_183349) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.citext "first_name"
+    t.citext "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_183349) do
   create_table "invoices", force: :cascade do |t|
     t.bigint "customer_id"
     t.bigint "merchant_id"
-    t.string "status"
+    t.citext "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
@@ -44,7 +45,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_183349) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string "name"
+    t.citext "name"
     t.text "description"
     t.float "unit_price"
     t.bigint "merchant_id"
@@ -54,16 +55,16 @@ ActiveRecord::Schema.define(version: 2019_11_19_183349) do
   end
 
   create_table "merchants", force: :cascade do |t|
-    t.string "name"
+    t.citext "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "invoice_id"
-    t.string "credit_card_number"
-    t.string "credit_card_expiration_date"
-    t.string "result"
+    t.citext "credit_card_number"
+    t.citext "credit_card_expiration_date"
+    t.citext "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
